2f5b95910fd16e59d7c9181008827a3d994f16c2
tsaulon <tyrone.saulon@hotmail.com>
2018-06-04 16:44:10 -0400

    Exception Handling Examples

---
+++ WEB322/w2/7_Exception_Handling.js
@@ -0,0 +1,65 @@
+//Design Tip:
+//It is important to never let your program suddenly crash or enter an unknown state due to an unanticipated error. 
+
+//Example:
+//Global isNaN(); is a way to respond to a situation which a number was expected
+
+
+/************************************
+ *      Handling Logic Errors       *
+ ************************************/
+let x = "twenty";
+let y = parseInt(x);
+
+if(isNaN(y))
+{
+    console.log("x cannot be converted to a number.");
+}
+else
+{
+    console.log("Success!, the numeric value of x is: " + y);
+}
+
+let a = 30, b = 0;
+let c = a / b;
+
+if(isFinite(c))
+{
+    console.log("Success!" + a + "/" + b + "=" + c);
+}
+else
+{
+    console.log(a + " is not divisible by " + b);
+}
+
+console.log();
+
+
+
+/**************************************************
+ *  Handling Errors That May Crash Program Pt. 1  *
+ **************************************************/
+ const PI = 3.14159;
+
+ try
+ {
+    console.log("(try) Attemptings to change PI");
+    PI = 99;
+    console.log("Haha! PI is now: " + PI);
+ }
+ catch(e)
+ {
+     console.log("(catch) Uh oh, an error occured: [" + e.message + "]");
+     console.log("(catch) Alas, it cannot be done, PI remains: " + PI);
+ }
+ finally
+ {
+     /************************************************
+     *  Handling Errors That May Crash Program Pt. 2 *
+     *************************************************/
+
+     //Note: code in this block will always be executed even if an exception was or wasn't caught.
+     console.log("(finally) Always execute code in this block.");
+ }
+
+ console.log();

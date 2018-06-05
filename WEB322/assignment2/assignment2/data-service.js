424763df1d2a26d2f1d123cda79f1ecbe16179e4
tsaulon <tyrone.saulon@hotmail.com>
2018-06-04 20:42:45 -0400

    Assignment 2 Completion

--- WEB322/assignment2/assignment2/data-service.js
+++ WEB322/assignment2/assignment2/data-service.js
@@ -1,3 +1,31 @@
+/*
+
+var path = require("path");
+
+module.exports.getEmployees = function(){
+    //setting variable to contents of .json file
+    //Note: "require" is synchronous and only reads the file once, following calls return the results from cache.
+    var employees = require(path.join(__dirname, "/data/employees.json"));
+
+    return employees;
+};
+
+module.exports.getDepartments = function(){
+    var departments = require(path.join(__dirname, "/data/departments.json"));
+
+    return departments;
+}
+
+module.exports.getManagers = function(){
+    var managers = require(path.join(__dirname, "/data/employees.json"));
+    
+    return managers[0];
+}
+
+//TODO: Resolve correct implementations for the above functions.
+//      Read over rest of week 3 for advanced JavaScript.
+
+*/
 var path = require("path");
 
 var employees = [];

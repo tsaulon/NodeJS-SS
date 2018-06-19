70ecd9d9f80b4619665c8ce7b63198832c6a48f9
tsaulon <tyrone.saulon@hotmail.com>
2018-06-19 15:50:25 -0400

    WEB322 assignment 3 completion upload

--- WEB322/assignment3/data-service.js
+++ WEB322/assignment3/data-service.js
@@ -4,8 +4,7 @@ var employees = [];
 var departments = [];
 
 //fill global array variables
-module.exports.Initialize = () =>
-{
+module.exports.Initialize = () => {
     return new Promise((resolve, reject) => {
         try{
             employees = require(path.join(__dirname, "/data/employees.json"));
@@ -25,23 +24,82 @@ module.exports.Initialize = () =>
     });
 }
 
-module.exports.getAllEmployees = () =>
-{
+module.exports.getAllEmployees = () => {
     return new Promise((resolve, reject) => {
         (employees.length > 0) ? resolve(employees) : reject("Array is empty.");
     });
 }
 
-module.exports.getManagers = () =>
-{
+module.exports.getManagers = () => {
     return new Promise((resolve, reject) => {
         (employees.length > 0) ? resolve(employees.filter(x => x.isManager == true)) : reject("Array is empty.");
     });
 }
 
-module.exports.getDepartments = () =>
-{
+module.exports.getDepartments = () => {
     return new Promise((resolve, reject) => {
         (departments.length > 0) ? resolve(departments) : reject("Array is empty.");
+    });
+}
+
+module.exports.addEmployee = data => {
+    return new Promise((resolve, reject) => {
+        data.isManager = (data.isManager == undefined) ? false : true;
+        data.employeeNum  = employees.length + 1;
+        employees.push(data);
+        resolve("Employee succesfully added!");
+    });
+}
+
+module.exports.getEmployeesByStatus = status => {
+    return new Promise((resolve, reject) => {
+
+        if(employees.length === 0){
+            reject("No results returned.");
+        } else{
+            resolve(employees.filter(x => {
+                return x.status == status;
+            }));      
+        }
+
+    });
+}
+
+module.exports.getEmployeesByDepartment = dept => {
+    return new Promise((resolve, reject) => {
+
+        if(employees.length === 0){
+            reject("No results returned.");
+        } else{
+            resolve(employees.filter(x => {
+                return x.department == dept;
+            }));
+        }
+    });
+}
+
+module.exports.getEmployeesByManager = manager => {
+    return new Promise((resolve, reject) => {
+
+        if(employees.length === 0){
+            reject("No results returned.");
+        } else{
+            resolve(employees.filter(x => {
+                return x.employeeManagerNum == manager
+            }));
+        }
+    });
+}
+
+module.exports.getEmployeeByNum = num => {
+    return new Promise((resolve, reject) => {
+
+        if(employees.length === 0){
+            reject("No results returned.");
+        } else{
+            resolve(employees.filter(x => {
+                return x.employeeNum == num;
+            }));
+        }
     });
 }
--- WEB322/assignment3/server.js
+++ WEB322/assignment3/server.js
@@ -2,26 +2,25 @@
 var data_service = require("./data-service.js");
 
 const HTTP_PORT = process.env.PORT || 8080;
+const bodyParser = require("body-parser");
 const multer = require("multer");
 const express = require("express");
 const path = require("path");
 const app = express();
 const fs = require("fs");   //allow for reading of dir contents
 
-//setup storage destination and creation of filenames.
-const storage = multer.diskStorage({
-    destination: "./public/images/uploaded",
+const uploadDIR = "./public/images/uploaded";
+const storage = multer.diskStorage({    //setup storage destination and creation of filenames.
+    destination: uploadDIR,
     filename: function(req, file, cb) {
         cb(null, Date.now() + path.extname(file.originalname));
     }
 });
+const upload = multer({storage: storage});    //tell multer to use diskStorage func for naming files
 
-//tell multer to use diskStorage func for naming files
-var upload = multer({storage: storage});
 
-//added before routes 
-//allow access to static "public" resources
-app.use(express.static("public"));
+app.use(express.static("public"));  //allow access to static "public" resources
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     //send file using an absolute path name from root.
@@ -38,55 +37,83 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/departments", (req, res) => {
-    data_service.getDepartments().then(
-        (data) => {
-            res.setHeader("Content-Type", "application/json");
-            res.json(data)    //resolved
-        }
-    ).catch(
-        (data) => console.log(data) //rejected
-    );
+    data_service.getDepartments().then( data => {
+        res.setHeader("Content-Type", "application/json");
+        res.json(data)    
+    }).catch(console.err);
 });
 
 app.get("/employees", (req, res) => {
-    data_service.getAllEmployees().then(
-        (data) => {
-            res.setHeader("Content-Type", "application/json");
-            res.json(data)    //resolved
+    //  When url contains optional filters like '/employees?status=Full%20Time' 
+    //  reference req.query.[name of option] to access the filter requested
+
+    //check if length of keys inside object is 0
+    if(Object.keys(req.query).length != 0){        
+        
+        if(req.query.status){
+            //if a status filter has been requested...
+            data_service.getEmployeesByStatus(req.query.status).then(data => {
+                res.json(data);
+            }).catch(console.err);
+        } else if(req.query.department){
+            //if a department filter has been requested...
+            data_service.getEmployeesByDepartment(req.query.department).then(data => {
+                res.json(data);
+            }).catch(console.err);
+        } else if(req.query.manager){
+            //if a manager filter has been requsted...
+            data_service.getEmployeesByManager(req.query.manager).then(data => {
+                res.json(data);
+            })
         }
-    ).catch(
-        (data) => console.log(data) //rejected
-    );
+
+    } else{
+        data_service.getAllEmployees().then( data => {
+            res.setHeader("Content-Type", "application/json");
+            res.json(data)    
+        }).catch(console.err); 
+    }    
+});
+
+app.get("/employee/:value", (req, res) => {
+    data_service.getEmployeeByNum(req.params.value).then(data => {
+        res.json(data);
+    }).catch(console.err);
 });
 
 app.get("/managers", (req, res) => {
-    data_service.getManagers().then(
-        (data) => {
-            res.setHeader("Content-Type", "application/json");
-            res.json(data);     //resolved
-        }
-    ).catch(
-        (data) => console.log(data) //rejected
-    )
+    data_service.getManagers().then( data => {
+        res.setHeader("Content-Type", "application/json");
+        res.json(data);     //resolved
+    }).catch(console.err);
 });
 
 app.get("/employees/add", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/addEmployee.html"));
 });
 
+app.post("/employees/add", (req, res) => {
+    data_service.addEmployee(req.body).then(data => {
+        console.log(data);
+    }).catch(console.err);
+
+    res.redirect("/employees");
+});
+
 app.get("/images/add", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/addImage.html"));
 });
 
-//
 app.post("/images/add", upload.single("imageFile"), (req, res) => {
     res.redirect("/images");
 });
 
 app.get("/images", (req, res) => {
-    fs.readdir("./public/images/uploaded", (err, items) => {
+    //read contents of directory 'uploadDIR'
+    //respond with contents in JSON format.
+    fs.readdir(uploadDIR, (err, items) => {
         res.setHeader("Content-Type", "application/json");
-        res.json(items);
+        res.json({images: items});
     });
 });
 
